4ae2f2c5fd921e999eb3a606ae828f9958b55ab7
James Bridgwater Court <james.bcourt@outlook.com>
2024-03-11 16:31:50 +0000

    Prepare to deploy

---
+++ TravellerProfile/models.py
@@ -0,0 +1,21 @@
+from django.db import models
+from django.contrib.auth.models import User
+import cloudinary
+from cloudinary.models import CloudinaryField
+
+# Create your models here.
+class Traveller(models.Model):
+    """
+    Stores an individual traveller, related to Django User model
+    """
+    user = models.OneToOneField(User, 
+        on_delete=models.CASCADE,
+        primary_key=True,)
+    firstname = models.CharField(max_length=50)
+    lastname = models.CharField(max_length=50)
+    bio = models.TextField(max_length=500)
+    profile_photo = CloudinaryField('image', default='placeholder')
+    created_on = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.user}: {self.firstname} {self.lastname} | created {self.created_on}"
---
+++ TravellerProfile/views.py
@@ -0,0 +1,22 @@
+from django.shortcuts import render
+from django.views import generic
+from .models import Traveller
+
+# Create your views here.
+class TravellerList(generic.ListView):
+    """
+    Returns all Traveller profiles in :model:`TravellerProfile:Traveller`
+    and displays them in a page of six posts.
+    **Context**
+
+    ``queryset``
+        All published instances of :model:`blog.Post`
+    ``paginate_by``
+        Number of posts per page.
+
+    **Template:**
+
+    :template:`blog/index.html`
+    """
+    queryset = Traveller.objects.all()
+    template_name = "TravellerProfile/index.html"
